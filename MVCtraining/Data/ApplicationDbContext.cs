14fdf8f3f2551999e273aeb1956f347948e8843a
Alex VanLaningham <ajvanlaningham@gmail.com>
2022-01-03 13:49:58 -0500

    added connection strings, expanded notes, created db context but definitely got he connection string part confused. working on it.

---
+++ MVCtraining/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCtraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCtraining.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
