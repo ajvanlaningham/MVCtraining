5f6ddaa76cc8222af4c3887ff219d2381db76898
Alex VanLaningham <ajvanlaningham@gmail.com>
2022-01-04 11:32:57 -0500

    added the category class and migrated to db, created controller and view, updated view HTML to receive list object of models from the controller

---
+++ MVCtraining/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCtraining.Models;
+using MVCtraining.Data;
+
+namespace MVCtraining.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
